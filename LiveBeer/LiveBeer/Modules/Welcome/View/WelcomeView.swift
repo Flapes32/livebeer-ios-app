e5abc3b74103426b7618ca0178701ad7c472359a
Dennis Brown <brown_denny@icloud.com>
2026-02-28 17:57:56 +0300

    REFACTORED] Organize project folders for MVVM modules

--- LiveBeer/LiveBeer/Modules/Welcome/View/WelcomeView.swift
+++ LiveBeer/LiveBeer/Modules/Welcome/View/WelcomeView.swift
@@ -1,13 +1,6 @@
-//
-//  ContentView.swift
-//  LiveBeer
-//
-//  Created by  Apple on 27.02.2026.
-//
-
 import SwiftUI
 
-struct ContentView: View {
+struct WelcomeView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +13,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    WelcomeView()
 }
