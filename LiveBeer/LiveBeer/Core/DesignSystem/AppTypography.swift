add4b409880595214fbdbc9a1316394a6b91ddca
Dennis Brown <brown_denny@icloud.com>
2026-03-02 12:55:10 +0300

    [ADDED] Build core reusable UI components

---
+++ LiveBeer/LiveBeer/Core/DesignSystem/AppTypography.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+enum AppTypography {
+    static let titleLarge = Font.system(size: 34, weight: .bold)
+    static let titleMedium = Font.system(size: 28, weight: .semibold)
+    static let titleSmall = Font.system(size: 22, weight: .semibold)
+    static let bodyPrimary = Font.system(size: 16, weight: .regular)
+    static let bodySecondary = Font.system(size: 15, weight: .medium)
+    static let caption = Font.system(size: 13, weight: .regular)
+    static let button = Font.system(size: 16, weight: .semibold)
+}
